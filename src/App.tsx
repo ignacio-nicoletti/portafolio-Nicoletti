4f812e4b03104a3fe013a2711dbca13b2d2b1903
Jonathan <jalexisgarcia.91@gmail.com>
2023-02-07 11:52:39 -0300

    correcciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import 'react-toastify/dist/ReactToastify.css'
+import React from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+
+import { GlobalStyle } from './styles/global';
+import 'react-toastify/dist/ReactToastify.css';
+import { Main } from './components/Main/Main';
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
+          <GlobalStyle></GlobalStyle>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
