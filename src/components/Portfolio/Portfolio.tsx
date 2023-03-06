73e229b292c4e0c8d2993f45e77ed28872f162d4
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-03-05 23:01:07 -0300

    store con next js

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -166,6 +166,32 @@ export function Portfolio() {
             </footer>
           </div>
         </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://github.com/ignacio-nicoletti/storeAPP-nextJS" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visitar sitio" />
+                </a>
+
+              </div>
+            </header>
+            <div className="body">
+              <h3>Store con nextJS</h3>
+              <p>Aprendiendo nextJS cree una store en la ruta home una tienda y otras rutas con otras tiendas.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>NextJS</li>
+
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
 
 
 
